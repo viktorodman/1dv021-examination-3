6f867de440d3584a4ff85ed3837b30f3934d54ab
Viktor Ödman <odman94@gmail.com>
2019-12-30 00:01:08 +0100

    Now i got two paddles

--- src/js/apps/Pong/pong-paddle.js
+++ src/js/apps/Pong/pong-paddle.js
@@ -8,51 +8,80 @@
 const template = document.createElement('template')
 template.innerHTML = `
     <style>
-        :host {
-            position: absolute;
-            left: 40px;
-        }
-        .paddleWrapper {
-            width: 10px;
-            
-            /* background-color: #0074D9; */
-        }
-        .paddleTop {
-            width: 100%;
-            height: 20px;
-            background-color: red;
-        }
-        .paddleMiddle {
-            width: 100%;
-            height: 20px;
-            background-color: blue;
-        }
-        .paddleBottom {
-            width: 100%;
-            height: 20px;
-            background-color: green;
-        }
-    </style>
-    <div class="paddleWrapper">
-        <div class="paddleTop"></div>
-        <div class="paddleMiddle"></div>
-        <div class="paddleBottom"></div>
-    </div>
+    </style>    
 `
+// https://stackoverflow.com/questions/52128997/how-to-create-canvas-class-with-javascript-es6
 class PongPaddle extends window.HTMLElement {
   constructor () {
     super()
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
 
-    this._intervalID = undefined
-    this._paddle = this.shadowRoot.querySelector('.paddle')
+    this._height = 30
+    this._width = 10
+    this._paddleSpacing = 10
+    this.color = 'black'
+    this._tableSide = undefined
+
+    this._position = {
+      x: null,
+      y: null
+    }
+    this._myCanvas = {
+      canvasWidth: undefined,
+      canvasHeight: undefined
+    }
+  }
+
+  /**
+   * Get what attributes attributeChangedCallback should look for
+   *
+   * @readonly
+   * @static
+   * @memberof PongPaddle
+   */
+  static get observedAttributes () {
+    return ['paddleposition', 'canvaswidth', 'canvasheight']
+  }
+
+  /**
+   * Is called when some of the observed attributes is called
+   *
+   * @param {String} name the attribute name
+   * @param {String} oldValue old attribute value
+   * @param {String} newValue new attribute value
+   * @memberof PongPaddle
+   */
+  attributeChangedCallback (name, oldValue, newValue) {
+    if (name === 'paddleposition') {
+      this._tableSide = newValue
+    }
+    if (name === 'canvaswidth') {
+      this._myCanvas.canvasWidth = Number(newValue)
+    }
+    if (name === 'canvasheight') {
+      this._myCanvas.canvasHeight = Number(newValue)
+      this._position.y = (Number(newValue) / 2) - (this._height / 2)
+    }
   }
 
   connectedCallback () {
-    /*  const width = this._paddle.offsetWidth
-    console.log(width) */
-    /* this.dispatchEvent(new window.CustomEvent('altchange', { detail: { columns, rows } })) */
+
+  }
+
+  _render (canvas) {
+    if (this._tableSide === 'right') {
+      this._position.x = (this._myCanvas.canvasWidth - this._width) - this._paddleSpacing
+    }
+    if (this._tableSide === 'left') {
+      this._position.x = this._paddleSpacing
+    }
+
+    canvas.beginPath()
+    canvas.rect(this._position.x, this._position.y, this._width, this._height)
+    canvas.fillStyle = this.color
+    canvas.fill()
+    canvas.closePath()
   }
 }
 
--- src/js/apps/Pong/pong-table.js
+++ src/js/apps/Pong/pong-table.js
@@ -8,12 +8,26 @@
 const template = document.createElement('template')
 template.innerHTML = `
     <style>
-       
+        :host {
+        padding: 0;
+        margin: 0;
+        }
+       .table {
+        width: 100%;
+        height: 100%;
+        background-color: #3D9970;
+        display: block;
+        margin: auto;
+       }
     </style>
-   
+   <canvas class="table">
+       <pong-paddle class="paddleOne" paddleposition="right"></pong-paddle>
+       <pong-paddle class="paddleTwo" paddleposition="left"></pong-paddle>
+   </canvas>
 `
 /**
  * Represents a Pong Table
+ * REF: https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Create_the_Canvas_and_draw_on_it
  *
  * @class PongTable
  * @extends {window.HTMLElement}
@@ -28,6 +42,10 @@ class PongTable extends window.HTMLElement {
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
 
+    this._table = this.shadowRoot.querySelector('.table')
+    this.ctx = this._table.getContext('2d')
+    this._paddleOne = this.shadowRoot.querySelector('.paddleOne')
+    this._paddleTwo = this.shadowRoot.querySelector('.paddleTwo')
     this._twoPlayers = false
   }
 
@@ -64,7 +82,16 @@ class PongTable extends window.HTMLElement {
    * @memberof PongTable
    */
   connectedCallback () {
+    this._paddleOne.setAttribute('canvaswidth', this._table.width)
+    this._paddleOne.setAttribute('canvasheight', this._table.height)
+    this._paddleTwo.setAttribute('canvaswidth', this._table.width)
+    this._paddleTwo.setAttribute('canvasheight', this._table.height)
+    this._start()
+  }
 
+  _start () {
+    this._paddleOne._render(this.ctx)
+    this._paddleTwo._render(this.ctx)
   }
 }
 window.customElements.define('pong-table', PongTable)
